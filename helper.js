ed75f24e11649764afd0cdf576cf8ae175f4ede2
DarkMatterMatt <mattmoran2000@gmail.com>
2019-11-03 19:59:03 +1300

    Add helper function rolesToPermissions

--- helper.js
+++ helper.js
@@ -1,3 +1,6 @@
+// imports
+const constants      = require("./constants.js");
+
 // shorter alias for module.exports
 const e = module.exports;
 
@@ -18,4 +21,29 @@ e.permissionsToInt = (permissionsList) => {
 
     throw new Error("Could not process list of permissions. "
         + `Must be an array or integer, recieved '${permissionsList}'.`);
+};
+
+e.rolesToPermissions = (rolesMap) => {
+    // if we recieved a list of roles, turn them into a list of permissions
+    if (rolesMap instanceof Map) {
+        const permissionsList = [];
+
+        // loop through the roles and add permissions
+        Object.values(constants.roles).forEach(role => {
+            if (rolesMap.has(role.id)) {
+                permissionsList.push(...role.permissions);
+            }
+        });
+
+        // return array, without duplicates
+        return [...new Set(permissionsList)];
+    }
+
+    if (typeof rolesMap === "string") {
+        // we recieved a single role
+        return constants.roles.find(x => x.id === rolesMap).roles;
+    }
+
+    throw new Error("Could not process Map of roles. "
+        + `Must be a Map or role id string, recieved '${rolesMap}'.`);
 };
