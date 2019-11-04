0c0137d694db3f6aee3293072ee0f0caf6211fc6
DarkMatterMatt <mattmoran2000@gmail.com>
2019-11-04 20:11:30 +1300

    Add loadUser function

--- userManager.js
+++ userManager.js
@@ -1,5 +1,7 @@
 // imports
+const Collection = require("discord.js/src/util/Collection");
 const helper = require("./helper.js");
+const User = require("./User.js");
 
 // shorter alias for module.exports
 const e = module.exports;
@@ -12,7 +14,7 @@ let dbPool = null;
  *
  * @param  pool  A connection pool to the TMHI database.
  */
-e.initialize = async (dbPool_) => {
+e.initialize = (dbPool_) => {
     dbPool = dbPool_;
 };
 
@@ -93,4 +95,62 @@ e.getPermissions = async (id) => {
     }
 
     return rows[0].permissions;
+};
+
+/*
+ * Retrieve a Discord user's permissions from the database.
+ *
+ * @param    id               The user's Discord snowflake id.
+ * @returns  A User object, or false if the user does not exist.
+ */
+e.loadUser = async (id) => {
+    let rows;
+
+    // load user from database
+    [rows] = await dbPool.query(`
+        SELECT displayname, timezone
+        FROM users
+        WHERE discordid = ?
+    ;`, [id]);
+
+    // no user found
+    if (rows.length === 0) {
+        return false;
+    }
+    const [name, timezone] = rows[0];
+
+    // load roles for user
+    [rows] = await dbPool.query(`
+        SELECT roles.id as id, roles.name as name, roles.description as description
+        FROM userroles
+        JOIN roles ON userroles.roleid=roles.id
+        WHERE userroles.userid = ?
+    ;`, [id]);
+
+    const roles = new Collection();
+    rows.forEach(row => {
+        roles.set(rows.id, {
+            name:        row.name,
+            description: row.description,
+        });
+    });
+
+    // load permissions for user
+    [rows] = await dbPool.query(`
+        SELECT roles.id as id, roles.name as name, roles.description as description
+        FROM userroles
+        JOIN rolepermissions ON userroles.roleid=rolepermissions.id
+        JOIN permissions     ON rolepermissions.permissionid=permissions.id
+        WHERE userroles.userid = ?
+    ;`, [id]);
+
+    const permissions = new Collection();
+    rows.forEach(row => {
+        permissions.set(rows.id, {
+            name:        row.name,
+            description: row.description,
+        });
+    });
+
+    return new User(id, name, roles, timezone, permissions);
 };
