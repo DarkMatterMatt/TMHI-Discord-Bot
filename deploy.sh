9a77d3eac5be84c17adcc5f166a64cb546cbae08
DarkMatterMatt <mattmoran2000@gmail.com>
2019-11-07 23:43:26 +1300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-cd "$( dirname "${BASH_SOURCE[0]}" )"
-git fetch --all
-git checkout --force "origin/master"
-pm2 reload src/index.js
+
+# change directory to TMHI-Discord-Bot
+echo "Changing directory to $( dirname "${BASH_SOURCE[0]}" )..."    && \
+cd "$( dirname "${BASH_SOURCE[0]}" )"   && \
+
+# fetch and checkout changes
+echo -e "\nUpdating the bot..."         && \
+git fetch --all                         && \
+git checkout --force "origin/master"    && \
+
+# reload the bot
+echo -e "\nReloading the bot..."        && \
+export HOME="/var/www"                  && \
+pm2 reload src/index.js                 && \
+
+# done
+echo -e "\nUpdate completed successfully."
