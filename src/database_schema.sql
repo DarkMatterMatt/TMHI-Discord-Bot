9697a39322fc87afa0018e84d862c85b87d5a06c
DarkMatterMatt <mattmoran2000@gmail.com>
2019-12-23 14:27:44 +1300

    Reduce row lengths to work on 32bit systems
    Reduced size of textContent and timerFinishMessage in clocks to avoid exceeding 32bit row length limits

--- src/database_schema.sql
+++ src/database_schema.sql
@@ -130,11 +130,11 @@ CREATE TABLE clocks (
     guildid         VARCHAR(32)     NOT NULL        COMMENT 'Discord Snowflake',
     channelid       VARCHAR(32)     NOT NULL        COMMENT 'Discord Snowflake',
     messageid       VARCHAR(32)     DEFAULT ''      COMMENT 'Discord Snowflake',
-    textcontent     VARCHAR(8192)   DEFAULT 'HH:MM',
+    textcontent     VARCHAR(4096)   DEFAULT 'HH:MM',
     utcoffset       BIGINT          COMMENT 'Milliseconds offset',
     timefinish      BIGINT          COMMENT 'Milliseconds since Jan 1, 1970, 00:00:00.000 GMT',
     timestart       BIGINT          COMMENT 'Milliseconds since Jan 1, 1970, 00:00:00.000 GMT',
-    timerfinishmessage              VARCHAR(8192),
+    timerfinishmessage              VARCHAR(4096),
 
     FOREIGN KEY     (guildid)       REFERENCES guilds(id)   ON DELETE CASCADE,
     PRIMARY KEY     (guildid, channelid, messageid)
