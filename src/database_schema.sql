f1139810daa965d2d9a79c4ad96a20d5a832da7f
Matt Moran <15150339+DarkMatterMatt@users.noreply.github.com>
2020-08-04 23:45:17 +1200

    Add migration schema

--- src/database_schema.sql
+++ src/database_schema.sql
@@ -152,7 +152,7 @@ INSERT INTO settings (id, name, defaultvalue, comment) VALUES ('INITIATE_ROLE', 
 INSERT INTO settings (id, name, defaultvalue, comment) VALUES ('INITIATE_MESSAGE',          'Initiate Message',         "",     'The message to greet initiate members with');
 INSERT INTO settings (id, name, defaultvalue, comment) VALUES ('CONCLUDED_ROLE',            'Concluded Role',           "",     'The role to grant to new members');
 INSERT INTO settings (id, name, defaultvalue, comment) VALUES ('CONCLUDED_MESSAGE',         'Concluded Message',        "",     'The message to greet members with');
-INSERT INTO settings (id, name, defaultvalue, comment) VALUES ('SQUADLESS_ROLE',            'Squadless Role',           "",     'The role to grant to new members wihtout a squad');
+INSERT INTO settings (id, name, defaultvalue, comment) VALUES ('SQUADLESS_ROLE',            'Squadless Role',           "",     'The role to grant to new members without a squad');
 INSERT INTO settings (id, name, defaultvalue, comment) VALUES ('SQUADLESS_MESSAGE',         'Squadless Message',        "",     'The message to post to the squadless channel');
 INSERT INTO settings (id, name, defaultvalue, comment) VALUES ('SQUADLESS_CHANNEL',         'Squadless Channel',        "",     'The channel to post the squadless message into');
 INSERT INTO settings (id, name, defaultvalue, comment) VALUES ('VERIFIED_ROLE',             'Verified Role',            "",     'The role someone needs to be able to get the member role');
